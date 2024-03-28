124f7680aa440df34903e2643a589091d45e60ae
Yaroslav <yrmaksymiv@gmail.com>
2024-03-28 21:18:39 +0200

    fixing 10

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -159,7 +159,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
+# Static files s(CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
